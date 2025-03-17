7f728bb5a8300260e5b259b02416c2dec76e743c
koromax <49192068+koromax@users.noreply.github.com>
2025-03-17 22:04:49 +0300

    [lab 8] this is the only thing i'm good at, and i have to excel at it, otherwise there will be nothing left to make me wake up in the morning

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
+#include <algorithm>
 #include <cstring>
+#include <iostream>
+
+void Task1() {
+    /* 14. Пользователь вводит текст. Вывести на экран номера позиций в которых встречается символ @. */
+    std::string s;
+    std::cin >> s;
+    auto i = std::find(s.begin(), s.end(), '@');
+    while (i != s.end()) {
+        std::cout << std::distance(s.begin(), i) << ' ';
+    }
+    std::cout << '\n';
+}
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    // сюда решение
+    Task1();
     return 0;
 }
